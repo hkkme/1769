440df3751a98d30d597deee08eb694609d018374
apfel <zehnter.david@gmail.com>
2020-06-29 21:45:31 +0200

    update formhandler

--- netlifyfunctions/formhandler.js
+++ netlifyfunctions/formhandler.js
@@ -1,9 +1,32 @@
+const mailgun = require('mailgun-js');
+const apiKey = process.env.MAILGUN_API_KEY;
+const domain = process.env.MAILGUN_DOMAIN;
+
 exports.handler = async (event, context) => {
 
   try {
-    const payload = JSON.parse(event.body);
+    const mg = mailgun({ apiKey, domain });
+    const data = JSON.parse(event.body);
 
-    console.log('payload', payload);
+
+    console.log('data 1', data);
+
+
+    mg.messages().send(data, (error, body) => {
+
+        console.log('data 2', data);
+        console.log('body', body);
+
+        if (error) {
+            console.log('error:', error)
+
+
+
+        }
+
+    })
+
+
 
     return {
       statusCode: 200,
