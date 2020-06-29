3f13a6ad4e1997d3a94cb7e93806cd2dacce5f0e
apfel <zehnter.david@gmail.com>
2020-06-29 21:36:18 +0200

    update formhandler

--- components/formgenerator/index.vue
+++ components/formgenerator/index.vue
@@ -1,19 +1,53 @@
 <template>
-  <div class="simple-sign-up">
-
-    <input
-      placeholder="test"
-      @input="inputChange"
-      type="text"
-    />
-
-    <button @click="submitForm">button</button>
+  <div class="from-generator">
+    <div
+      v-for="(formItem, index) in formItems"
+      :key="index"
+    >
+      <input
+        :label="formItem.label"
+        :name="formItem.id"
+        :placeholder="formItem.placeholder"
+        @input="inputChange"
+        class="input-text"
+        type="input"
+        v-if="formItem.type === 'input'"
+      />
+      <textarea
+        :label="formItem.label"
+        :name="formItem.id"
+        :placeholder="formItem.placeholder"
+        :style="{ height: formItem.height + 'px' }"
+        @input="inputChange"
+        class="input-textarea"
+        v-if="formItem.type === 'textArea'"
+      />
+      <div
+          class="input-note"
+          v-html="formItem.value"
+          v-if="formItem.type === 'note'"
+      />
+      <input
+        :value="formItem.value"
+        @click="submitForm"
+        class="input-submit"
+        type="submit"
+        v-if="formItem.type === 'submit'"
+      />
+    </div>
+    <div
+      :class="message && message.type"
+      class="message"
+    >{{ message && message.text }}</div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'testform',
+    created() {
+      this.formItems = this.$props.data.formItems;
+    },
     props: {
       data: {
         type: Object,
@@ -22,24 +56,22 @@
     },
     data: () => {
       return {
-        formval: 'null',
-        emailNotValid: 'testing',
-        message: null,
-        messageColor: null,
+        formItems: [],
+        formValues: {},
+        message: null
       }
     },
     methods: {
       inputChange(event) {
-        // this.formval = event.target.value;
-        console.log(event.target.value)
+        this.formValues[event.target.name] = event.target.value;
       },
       submitForm() {
 
 
-      fetch('/.netlify/functions/formhandler', {
+        fetch('/.netlify/functions/formhandler', {
           method: 'post',
           body: JSON.stringify({
-            testval: this.formval
+            formValues: this.formValues
           })
         }).then(function(response) {
           return response.json();
@@ -58,3 +90,93 @@
   };
 </script>
 
+<style lang="scss">
+  @import '~assets/scss/main.scss';
+
+  .from-generator {
+    margin: 20px auto 100px auto;
+    max-width: 1000px;
+    width: calc(100% - 20px);
+
+    .input-text {
+      background-color: lightgrey;
+      border: 0;
+      color: black;
+      font-family: 'Courier Prime', monospace;
+      font-size: 20px;
+      margin-bottom: 10px;
+      padding: 5px;
+      text-transform: lowercase;
+      width: 700px;
+      &::placeholder {
+        color: $placeholder;
+      }
+    }
+
+    .input-textarea {
+      background-color: lightgrey;
+      border: 0;
+      color: black;
+      font-family: 'Courier Prime', monospace;
+      font-size: 20px;
+      height: 400px;
+      margin-bottom: 10px;
+      max-width: 700px;
+      min-width: 700px;
+      padding: 5px;
+      text-transform: lowercase;
+      width: 700px;
+    }
+
+    .input-note {
+      font-family: 'Courier Prime', monospace;
+      font-size: 16px;
+      padding: 0 5px;
+      margin-bottom: 30px;
+      width: 700px;
+      a {
+        color: red;
+        text-decoration: none;
+        &:hover {
+          background-color: red;
+          color: white;
+        }
+      }
+    }
+
+    .input-submit {
+      background-color: $blue;
+      border: 0;
+      color: lightgrey;
+      display: block;
+      font-family: 'Alata', sans-serif;
+      font-size: 20px;
+      margin: 15px 0 0 0;
+      padding: 10px;
+      &:hover {
+        border: 0;
+        color: white;
+        cursor: pointer;
+      }
+    }
+
+    .message {
+      padding: 5px;
+      margin: 10px 0;
+      width: 700px;
+      font-family: 'Courier Prime', monospace;
+      font-size: 24px;
+
+      &.warn {
+        color: red;
+      }
+
+      &.confirm {
+        color: green;
+      }
+
+    }
+
+  }
+
+</style>
