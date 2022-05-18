618b085222c0bb88f3bb8ca7d02f867d7084d9a5
hkkme <zehnter.david@gmail.com>
2022-05-18 15:44:15 +0200

    add concierge

--- components/navmap/index.vue
+++ components/navmap/index.vue
@@ -88,7 +88,7 @@
 
         this.graphLayout = d3
         .forceSimulation(this.navmapJson.nodes)
-        .force("charge", d3.forceManyBody().strength(-850)) // change here for distance between nodes
+        .force("charge", d3.forceManyBody().strength(-800)) // change here for distance between nodes
         .force("center", d3.forceCenter(this.width / 2, this.height / 2 - 50))
         .force("link", d3.forceLink(this.navmapJson.links)
           .id(function(d) {return d.id; })
